bf03551d4f030f031f8e0f04e9b1e9f982677c42
rdeng <rdeng@tencent.com>
2017-10-12 14:53:25 +0800

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public GUIText countTxt;
+    private int count;
+    private Rigidbody body;
+
+    // Use this for initialization
+    void Start () {
+        count = 0;
+        countTxt.text = "";
+        body = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        body.AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        if (other.gameObject.tag == "PickUp")
+        {
+            //Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+            count++;
+            countTxt.text = "Count: " + count.ToString();
+        }
+    }
+}
